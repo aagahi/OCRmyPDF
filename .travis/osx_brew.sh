adc132fc349ef2ca776e2b683c4b961c8c9ab599
James R. Barlow <jim@purplerock.ca>
2017-04-19 12:48:55 -0700

    Try using ssh-agent to authorize key

--- .travis/osx_brew.sh
+++ .travis/osx_brew.sh
@@ -16,16 +16,11 @@ openssl aes-256-cbc -K $encrypted_e35043491734_key -iv $encrypted_e35043491734_i
 set -x
 
 chmod 400 homebrew-ocrmypdf_key
-ssh-add -K homebrew-ocrmypdf_key
-export GIT_SSH_COMMAND="ssh -i homebrew-ocrmypdf_key -F /dev/null"
-
-chmod a+rx .travis/ssh-askpass
-export SSH_ASKPASS="$(pwd)/.travis/ssh-askpass"
-
-git clone git@github.com:jbarlow83/homebrew-ocrmypdf.git homebrew
+ssh-agent sh -c 'ssh-add homebrew-ocrmypdf_key; git clone git@github.com:jbarlow83/homebrew-ocrmypdf.git homebrew'
 
 cd homebrew
 cp ../ocrmypdf.rb Formula/ocrmypdf.rb
 git add Formula/ocrmypdf.rb
 git commit -m "homebrew-ocrmypdf: automatic release $TRAVIS_BUILD_NUMBER $TRAVIS_TAG"
-git push -v origin master
+
+ssh-agent sh -c 'ssh-add homebrew-ocrmypdf_key; git push -v origin master'
