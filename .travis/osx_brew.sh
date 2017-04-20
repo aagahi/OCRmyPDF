cabc1d7cd64d93762cf89b5b6c6cfbaed9346eb4
James R. Barlow <jim@purplerock.ca>
2017-04-20 00:16:14 -0700

    Try using OAuth instead of SSH keys

--- .travis/osx_brew.sh
+++ .travis/osx_brew.sh
@@ -7,20 +7,14 @@ python3 .travis/autobrew.py
 cat ocrmypdf.rb
 brew audit ocrmypdf.rb
 
-# Extract deploy key for jbarlow83/homebrew-ocrmypdf
-# Disable debug to avoid outputting key
-echo "Decrypting .travis/homebrew-ocrmypdf.enc"
+# Important: disable debug output so token is hidden
 set +x
-openssl aes-256-cbc -K $encrypted_e35043491734_key -iv $encrypted_e35043491734_iv \
-    -in .travis/homebrew-ocrmypdf.enc -out homebrew-ocrmypdf_key -d
+git clone https://$HOMEBREW_OCRMYPDF_TOKEN@github.com/jbarlow83/homebrew-ocrmypdf.git
 set -x
 
-chmod 400 homebrew-ocrmypdf_key
-ssh-agent sh -c 'ssh-add homebrew-ocrmypdf_key; git clone git@github.com:jbarlow83/homebrew-ocrmypdf.git homebrew'
-
-cd homebrew
+pushd homebrew-ocrmypdf
 cp ../ocrmypdf.rb Formula/ocrmypdf.rb
 git add Formula/ocrmypdf.rb
 git commit -m "homebrew-ocrmypdf: automatic release $TRAVIS_BUILD_NUMBER $TRAVIS_TAG"
-
-ssh-agent sh -c 'ssh-add homebrew-ocrmypdf_key; git push -v origin master'
+git push origin master
+popd
